83a332c13ec55620f8da09d65d6f1da4248129f8
Francisco Cea <fcoceak@gmail.com>
2024-01-19 17:28:31 -0300

    refactor: add error checks and SSH port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+if [ -z "${SSH_HOST}" ]; then
+  echo "ERROR | SSH_HOST is not set."
+  exit 1
+fi
+if [ -z "${SSH_PASS}" ]; then
+  echo "ERROR | SSH_PASS is not set."
+  exit 1
+fi
+if [ -z "${SSH_USER}" ]; then
+  echo "ERROR | SSH_USER is not set."
+  exit 1
+fi
+
+if [ -z "${FOLDER}" ]; then
+  echo "ERROR | FOLDER is not set."
+  exit 1
+fi
+
+if [ -z "${PM2_SERVICE}" ]; then
+  echo "ERROR | PM2_SERVICE is not set."
+  exit 1
+fi
+
 PNPM="/${SSH_USER}/.local/share/pnpm/pnpm"
 SSH_COMMANDS=$(cat << EOF
 if [ ! -d "${FOLDER}" ]; then
@@ -12,10 +35,10 @@ if [ "\${PM2_EXIST}" -eq 0 ]; then
   echo "ERROR | PM2 service \${PM2_SERVICE} does not exist."
   exit 1
 fi
-# if ! git rev-parse --verify ${REPO_BRANCH} > /dev/null 2>&1; then
-#   echo "ERROR | Branch ${REPO_BRANCH} does not exist. Exiting."
-#   exit 1
-# fi
+if ! git rev-parse --verify ${REPO_BRANCH} > /dev/null 2>&1; then
+  echo "ERROR | Branch ${REPO_BRANCH} does not exist. Exiting."
+  exit 1
+fi
 git checkout \${REPO_BRANCH}
 git pull
 ${PNPM} i
@@ -25,7 +48,7 @@ echo "SUCCESS | Deployment completed."
 EOF
 )
 
-sshpass -p ${SSH_PASS} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SSH_USER}@${SSH_HOST} "$SSH_COMMANDS"
+sshpass -p ${SSH_PASS} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SSH_USER}@${SSH_HOST}:${SSH_PORT} "$SSH_COMMANDS"
 SSH_EXIT_CODE=$?
 if [ $SSH_EXIT_CODE -ne 0 ]; then
   echo "SSH command failed. Exiting."
