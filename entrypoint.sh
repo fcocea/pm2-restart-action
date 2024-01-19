31af6b893e770102ccb2d9f279875fb05920c287
Francisco Cea <fcoceak@gmail.com>
2024-01-19 13:12:22 -0300

    fix: fix cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CMD="cd ${FOLDER} git checkout ${REPO_BRANCH} && git pull origin ${REPO_BRANCH} && pnpm i && pnpm build && pm2 restart ${PM2_SERVICE}"
+CMD="cd ${FOLDER} && git checkout ${REPO_BRANCH} && git pull origin ${REPO_BRANCH} && pnpm i && pnpm build && pm2 restart ${PM2_SERVICE}"
 
 sshpass -p "${SSH_PASS}" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${SSH_USER}@${SSH_HOST}" ${CMD}
